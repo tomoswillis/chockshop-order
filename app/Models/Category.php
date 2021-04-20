f88f9df1473e79916cddbf722e6a773e453859e7
Tomos <Willistomos@gmail.com>
2021-04-20 21:28:04 +0100

    added things

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,16 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
+class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'title',
-    ];
+    protected $guarded = [];
 
-    public function roles()
+    public function products()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Product::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -73,4 +73,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
