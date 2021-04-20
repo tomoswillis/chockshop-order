6296ce72b9b7324f3e1cbcd1c595a5e43077369b
Tomos <Willistomos@gmail.com>
2021-04-20 21:25:45 +0100

    adds cart to inertia

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -37,7 +38,9 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'cart' =>   $request->session()->get('cart'),
+            // 'product-access' =>   Auth::user()->get('role'),
+            // 'user-access' =>   Auth::user()->can('users_access'),
         ]);
     }
 }
