2291edae3263b9e326da2cfcd91d9730be674127
Tomos <Willistomos@gmail.com>
2021-04-28 16:00:54 +0100

    order mail model

---
+++ app/Mail/OrderReceived.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderReceived extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $orderedItems;
+    // public $totalCost;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($orderedItems)
+    {
+
+        // $this->$totalCost;
+        $this->orderedItems = $orderedItems;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.received');
+    }
+}
