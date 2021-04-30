1ea0a9b3d1de808885b3c4f8bf7da2e8d4531635
Tomos <Willistomos@gmail.com>
2021-04-30 12:47:37 +0100

    adds typeface

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://use.typekit.net/tph1eda.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
