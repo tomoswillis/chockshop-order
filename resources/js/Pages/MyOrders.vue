27cb1dc052c3ae102d12681d261db8b7b1498c77
Tomos <Willistomos@gmail.com>
2021-05-07 03:02:15 +0100

    breakes out comps

--- resources/js/Pages/MyOrders.vue
+++ resources/js/Pages/MyOrders.vue
@@ -1,25 +1,33 @@
 <template>
     <app-layout>
-
+        <div class="flex pt-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  checkoutImg">
+            <div class=" w-1/3">
+               
+                <single-order v-for="order in orders" key="order.id" :order="order"/>
+            </div>
+            <div class=" w-2/3 text-white mt-5" v-if="selected">
+                <selectedOrder :order="selected"/>
+            </div>
+                
+        </div>
 
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import SingleOrder from '@/Cards/SingleOrder'
-    import SelectedOrder from '@/Cards/SelectedOrder'
-    import Status from '@/Elements/Status'
+    import SingleOrder from '@/Cards/SingleMyOrder'
+    import SelectedOrder from '@/Cards/SelectedMyOrder'
 
     export default {
         components: {
             AppLayout,
             SingleOrder,
             SelectedOrder,
-            Status,
         },
         props: {
             orders: null,
+            selected: null,
         },
 
        
@@ -29,7 +37,7 @@
         },
         
         methods: {
-           
+          
         },
 
         watch: {
