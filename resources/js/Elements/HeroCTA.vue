906e7609a6f38445f21314ea81bd60537c35e80a
Tomos <Willistomos@gmail.com>
2021-05-28 03:13:50 +0100

    cleanup

---
+++ resources/js/Elements/HeroCTA.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="flex flex-col md:flex-row justify-center items-center relative">
+    <div
+      class="h-64 w-64 z-10 transform translate-y-14 md:translate-y-0 md:translate-x-10 lg:w-80 lg:h-80"
+    >
+      <img id="image" class="" :src="img" alt="hero image" />
+    </div>
+    <div
+      class="h-80 w-80 border-4 border-chock flex flex-col justify-center text-center p-5 lg:p-10 md:w-96 md:h-96 lg:w-104 lg:h-104 relative"
+    >
+      <img
+        src="images/franchise/vectors/theImportantStuff.svg"
+        class="absolute -bottom-1/4 left-20 w-64 lg:left-36"
+        alt=""
+      />
+      <h2
+        id="message"
+        class="text-white font-chockshop text-2xl md:text-3xl uppercase"
+        v-text="message"
+      />
+      <chock-shop-button class="mt-5 md:mx-10">
+        {{ buttonText }}</chock-shop-button
+      >
+    </div>
+  </div>
+</template>
+<script>
+import ChockShopButton from "@/Elements/ChockShopButton";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default {
+  components: {
+    ChockShopButton,
+  },
+
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      "#message",
+      {
+        y: 100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scrollTrigger: "#message",
+        duration: 1,
+      }
+    );
+
+    if (screen.width >= 768) {
+      gsap.fromTo(
+        "#image",
+        {
+          x: 100,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          scrollTrigger: "#image",
+          duration: 4,
+          duration: 1.5,
+          ease: "power2.inOut",
+        }
+      );
+    } else {
+      gsap.fromTo(
+        "#image",
+        {
+          y: -100,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          scrollTrigger: "#image",
+          duration: 1.2,
+        }
+      );
+    }
+  },
+
+  props: {
+    link: String,
+    buttonText: String,
+    message: String,
+    img: String,
+  },
+};
+</script>
+<style scoped>
+.before-enter {
+  opacity: 0;
+  transform: translateY(100px);
+  transition: all 2s ease-in-out;
+}
+.enter {
+  opacity: 1;
+  transform: translateY(0px);
+}
+</style>
--- resources/js/Sections/Header.vue
+++ resources/js/Sections/Header.vue
@@ -6,7 +6,9 @@
       <div
         class="title text-white font-chockshop uppercase z-10 text-4xl md:text-6xl lg:text-7xl xl:text-6xl flex items-end"
       >
-        Interested in a tasty franchise opportunity?
+        <transition appear @before-enter="beforeTitle" @enter="titleEnter">
+          <h1>Interested in a tasty franchise opportunity?</h1>
+        </transition>
       </div>
       <div class="text text-white text-light z-10 relative">
         <p class="my-4 text-md">
@@ -14,7 +16,7 @@
           chocolate and many other finest ingredients, we produce the UK’s most
           sumptuous selection of chocolate brownies.
         </p>
-        <chock-shop-button class="my-10 md:mb-8">
+        <chock-shop-button class="my-10 md:mb-8 md:w-1/3">
           Contact Us
         </chock-shop-button>
         <img
@@ -23,13 +25,22 @@
           alt="You know you want to"
         />
       </div>
-      <div class="image bg-chock overflow-hidden">
-        <img
-          class="w-full image md:rounded-bl-3xl shadow-chock-header filter-brightness-25 md:filter-brightness-50 lg:filter-brightness-100"
-          src="images/franchise/brownieHeaderImage.png"
-          alt="Hero image"
-        />
-      </div>
+      <transition
+        appear
+        @before-enter="beforeBrownieImage"
+        @enter="brownieImageEnter"
+      >
+        <div class="image bg-chock overflow-hidden">
+          <div
+            class="absolute bg-chock h-full transform translate-x-full w-full z-10"
+          />
+          <img
+            class="w-full image md:rounded-bl-3xl shadow-chock-header filter-brightness-25 md:filter-brightness-50 lg:filter-brightness-100"
+            src="images/franchise/brownieHeaderImage.png"
+            alt="Hero image"
+          />
+        </div>
+      </transition>
       <div
         class="Fran text-white flex justify-center flex-col rounded-3xl md:rounded-tl-none md:rounded-bl-none rounded-tr-3xl rounded-br-3xl lg:rounded-tr-none lg:rounded-br-none lg:rounded-tl-3xl lg:rounded-bl-3xl py-5 pl-5 pr-7 z-30"
       >
@@ -48,9 +59,39 @@
 
 <script>
 import chockShopButton from "@/Elements/ChockShopButton";
+import gsap from "gsap";
+
 export default {
   components: {
     chockShopButton,
+  },
+  setup() {
+    const beforeTitle = (el) => {
+      el.style.transform = "translateY(60px)";
+      el.style.opacity = 0;
+    };
+
+    const titleEnter = (el) => {
+      gsap.to(el, {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+      });
+    };
+
+    const beforeBrownieImage = (el) => {
+      el.style.transform = "translateX(50px)";
+      el.style.opacity = 0;
+    };
+
+    const brownieImageEnter = (el) => {
+      gsap.to(el, {
+        translateX: 0,
+        opacity: 1,
+        duration: 1.2,
+      });
+    };
+    return { beforeTitle, titleEnter, beforeBrownieImage, brownieImageEnter };
   },
 };
 </script>
