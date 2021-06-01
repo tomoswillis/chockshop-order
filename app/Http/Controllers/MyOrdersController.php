c0cae9a95a225ada685df2d42331b3e6d7261a68
Tomos <Willistomos@gmail.com>
2021-06-01 19:32:56 +0100

    chanages Abort to 'if' from try catch block

--- app/Http/Controllers/MyOrdersController.php
+++ app/Http/Controllers/MyOrdersController.php
@@ -12,11 +12,11 @@ use PhpParser\Node\Stmt\TryCatch;
 class MyOrdersController extends Controller
 {
     public function index(Request $request)
-    {   
+    {
         $id = $request->user()->id;
 
         // dd();
-       
+
         return Inertia::render('MyOrders', [
             'orders' => Order::where('user_id', $id)->with(['products:id,name', 'status:id,name'])->get(),
         ]);
@@ -25,7 +25,7 @@ class MyOrdersController extends Controller
 
     public function update(Request $request)
     {
-        
+
 
         return  Redirect::route('orders.index');
     }
@@ -35,21 +35,19 @@ class MyOrdersController extends Controller
         $userId = $request->user()->id;
         $order = Order::where('id', $id)->with('user:id')->get('user_id');
 
-        try {
-            $userId !== $order[0]->user_id;
-        } catch (\Throwable $th) {
+        if ($userId !== $order[0]->user_id) {
             abort(403);
+        } else {
 
+            return Inertia::render('MyOrders', [
+                'orders' => Order::where('user_id', $userId)->with(['products:id,name', 'status:id,name'])->get(),
+                'selected' => Order::where('id', $id)->with(['products', 'status:id,name'])->get(),
+            ]);
         }
-        
-        return Inertia::render('MyOrders', [
-            'orders' => Order::where('user_id', $userId)->with(['products:id,name', 'status:id,name'])->get(),
-            'selected' => Order::where('id', $id)->with(['products', 'status:id,name'])->get(),
-        ]);
     }
-   
 
-    
+
+
     public function destroy(Order $order)
     {
         $order->delete();
