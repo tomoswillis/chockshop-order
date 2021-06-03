c549a05c6c23b75cc71fab36f54a6a82ad7012d6
Tomos <Willistomos@gmail.com>
2021-06-03 01:19:42 +0100

    name update

--- resources/js/Cards/ContactForm.vue
+++ resources/js/Cards/ContactForm.vue
@@ -2,7 +2,7 @@
   <transition name="contact">
     <div
       id="contactBox"
-      class="fixed bottom-0 w-screen h-screen z-50 p-5 flex items-end md:items-center lg:justify-center foobar"
+      class="fixed bottom-0 w-screen h-screen z-50 p-5 flex items-end md:items-center lg:justify-center contactBox"
     >
       <div
         class="bg-blur-chock w-full rounded-xl px-5 pb-5 pt-16 relative xl:w-1/3"
@@ -91,7 +91,7 @@ export default {
   mounted() {},
   computed: {
     contactLeave: function () {
-      gsap.to(".foobar", {
+      gsap.to(".contactBox", {
         translateY: -100,
         opacity: 0,
         duration: 0.5,
