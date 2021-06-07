7dbbc7b2c2036213f1ffcf7cc534e6d4cf3d3467
Tomos <Willistomos@gmail.com>
2021-06-07 21:35:06 +0100

    links props

--- resources/js/Elements/ChockShopButton.vue
+++ resources/js/Elements/ChockShopButton.vue
@@ -1,19 +1,21 @@
 <template>
   <div
-    id="button"
-    @mouseover="foobar"
     class="py-2 px-4 border-2 rounded-lg border-red-300 uppercase font-bold text-xs text-red-300 text-center hover:bg-red-300 hover:text-white cursor-pointer"
   >
-    <slot></slot>
+    <inertia-link id="button" :href="link">
+      <slot></slot>
+    </inertia-link>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    // foobar() {
-    //   document.getElementById("button").classList.add("bg-black");
-    // },
+  methods: {},
+  props: {
+    link: {
+      type: String,
+      default: "/",
+    },
   },
 };
 </script>
