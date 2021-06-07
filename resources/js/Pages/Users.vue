87e6c5f5e705badc59db13ea474a665f8d4b0fd8
Tomos <Willistomos@gmail.com>
2021-06-07 21:34:44 +0100

    removes title

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -22,9 +22,7 @@
                     <th
                       scope="col"
                       class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Title
-                    </th>
+                    ></th>
                     <th
                       scope="col"
                       class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
