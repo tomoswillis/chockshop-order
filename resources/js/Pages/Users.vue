26323e194fdcb2bd74c4929caa00009ddab824e9
Tomos <Willistomos@gmail.com>
2021-06-07 14:23:04 +0100

    cleans table

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,102 +1,153 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-               Franchises
-            </h2>
-        </template>
-        <!-- This example requires Tailwind CSS v2.0+ -->
-        <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
-                        <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Name
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Title
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Status
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            
-                        </th>
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        <tr v-for="user in users" :key="user.id">
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            <div class="flex items-center">
-                            <div class="flex-shrink-0 h-10 w-10">
-                                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                            </div>
-                            <div class="ml-4">
-                                <div class="text-sm font-medium text-gray-900">
-                                
-                                </div>
-                                <div class="text-sm text-gray-500">
-                                jane.cooper@example.com
-                                </div>
-                            </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            <div class="text-sm text-gray-900">{{ user.email }}</div>
-                            <div class="text-sm text-gray-500">Optimization</div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            Active
-                            </span>
-                        </td>
-                        <td  v-for="role in user.roles" :key="role.id"  class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                            {{ role.title }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            <inertia-link :href="route('users.show', user.id)" class="text-indigo-600 hover:text-indigo-900">show</inertia-link>
-                        </td>
+  <app-layout>
+    <div class="pt-24"></div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+          >
+            <div
+              class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+            >
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Status
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Role
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-slate divide-y divide-gray-200">
+                  <tr v-for="person in users" :key="person.email">
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="ml-4">
+                          <div class="text-sm font-medium text-white">
+                            {{ person.name }}
+                          </div>
+                          <div class="text-sm text-gray-200">
+                            {{ person.email }}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">
+                        {{ person.title }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ person.department }}
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <span
+                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                      >
+                        Active
+                      </span>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                      {{ person.roles[0].title }}
+                    </td>
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                    >
+                      <inertia-link
+                        :href="route('users.show', person.id)"
+                        class="text-chock hover:text-chock-dark"
+                        >show</inertia-link
+                      >
+                    </td>
 
-                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            <inertia-link :href="route('users.edit', user.id)" class="text-indigo-600 hover:text-indigo-900">edit</inertia-link>
-                        </td>
-                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            <inertia-link :href="route('users.destroy', user.id )"   class="text-indigo-600 hover:text-indigo-900">delete</inertia-link>
-                              
-                        </td>
-
-                        </tr>
-
-                        <!-- More people... -->
-                    </tbody>
-                    </table>
-                </div>
-                </div>
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                    >
+                      <inertia-link
+                        :href="route('users.edit', person.id)"
+                        class="text-chock hover:text-chock-dark"
+                        >edit</inertia-link
+                      >
+                    </td>
+                    <td
+                      class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                    >
+                      <inertia-link
+                        :href="route('users.destroy', person.id)"
+                        class=""
+                        ><div
+                          class="hover:bg-red-300 p-3 rounded hover:text-red-900 text-white flex flex-col items-center"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="15"
+                            height="15"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-trash"
+                          >
+                            <polyline points="3 6 5 6 21 6"></polyline>
+                            <path
+                              d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+                            ></path>
+                          </svg></div
+                      ></inertia-link>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+          </div>
         </div>
-
-
-    </app-layout>
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
+import AppLayout from "@/Layouts/AppLayout";
 
-    export default {
-        
-        components: {
-            AppLayout,
-        },
+export default {
+  components: {
+    AppLayout,
+  },
 
-          props: {
-            users: Array,
-        },
-
-    }
+  props: {
+    users: Array,
+  },
+};
 </script>
